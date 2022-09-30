51afa6263387b8e0eddfe2b8519e324114579b79
Valdemir Filho <filhux@gmail.com>
2022-09-30 19:53:26 -0300

    Add json de exemplo

--- pages/[user].js
+++ pages/[user].js
@@ -1,6 +1,7 @@
 import styles from '../styles/User.module.css'
 import Image from 'next/image'
 import fs from 'fs'
+import Link from 'next/link'
 
 export async function getStaticPaths() {
   const files = listFiles()
@@ -39,41 +40,47 @@ export async function getStaticProps(context) {
 export default function User({user}) {
 
   return (
-    <>
-      <div className={styles.card}>
-        <div className={styles.user_info}>
-          <Image 
-            className={styles.card_image} 
-            src={`${user.github}.png`} 
-            width={150} height={150} 
-            alt={user.name}
-          />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-        </div>
+    <div className={styles.card}>
+      <Link href="/">Home</Link>
+      <div className={styles.user_info}>
+        <Image
+          className={styles.card_image}
+          src={`${user.github}.png`}
+          width={150} height={150}
+          alt={user.name}
+        />
+        <h2>{user.name}</h2>
+        <p>{`${user.city} - ${user.state}`}</p>
+      </div>
 
-        <div className={styles.links}>
-          { user.github && 
+      <div className={styles.links}>
+        {user.email &&
           <div className={styles.link}>
-            <p>Github:</p>
+            <p>Email</p>
+            <a href={`mailto:${user.email}`}>{user.email}</a>
+          </div>}
+
+        {user.github &&
+          <div className={styles.link}>
+            <p>Github</p>
             <a href={user.github}>{user.github}</a>
-          </div> }
+          </div>}
 
-          { user.linkedin_url && 
+        {user.linkedin_url &&
           <div className={styles.link}>
-            <p>Linkedin:</p>
+            <p>Linkedin</p>
             <a href={user.linkedin_url}>{user.linkedin_url}</a>
-          </div> }
+          </div>}
 
-          { user.rocketseat_profile &&
+        {user.rocketseat_profile &&
           <div className={styles.link}>
-            <p>Rocketseat:</p>
+            <p>Rocketseat</p>
             <a href={user.rocketseat_profile}>{user.rocketseat_profile}</a>
-          </div> }
-        </div>
+          </div>}
 
       </div>
-    </>
+
+    </div>
   )
 
 }
