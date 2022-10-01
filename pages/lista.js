d5c360efc792a57bc96b571e49568eae382148de
Valdemir Filho <filhux@gmail.com>
2022-10-01 02:48:16 -0300

    teste 2

---
+++ pages/lista.js
@@ -0,0 +1,59 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+import fs from 'fs'
+
+export async function getStaticProps() {
+  const users = listFiles()
+
+  // const user = await import("../users/danilo2.js")
+  // console.log("user:", user.data)
+  
+  const data = await Promise.all(users.map(async (item) => {
+    const user = await import(`../users/${item}`)
+    // console.log("user::", user.data)
+    return user.data
+
+    // const json = fs.readFileSync(`database/${item}`)
+    // return (JSON.parse(json))
+  }))
+  
+  console.log("data:", data)
+  
+  function listFiles() {
+    const files = fs.readdirSync("users")
+    return files
+  }
+
+  return {
+    props: { data }
+  }
+}
+
+export default function Home({data}) {
+  return (
+    <div className={styles.page}>
+    <Head>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+    <h1 className={styles.title}>🚀 <span>DEV</span>EXPLORER </h1>
+      <div className={styles.cards}>
+        {data.map((user, index) => (
+          <Link key={index} href={`/${user.github.split("/")[3]}`}>
+              <div className={styles.card}>
+                <div>
+                  <Image className={styles.card_image}
+                    src={`${user.github}.png`} width={80} height={80} alt={user.name} />
+                </div>
+                <h2>{user.name}</h2>
+                {/* <p>Cidade - Estado</p> */}
+              </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ users/danilo2.js
@@ -0,0 +1,9 @@
+export const data = {
+  name: "Danilo Cecci",
+  email: "danilocecci@gmail.com",
+  github: "https://github.com/danilocecci",
+  city: "Andradina",
+  state: "São Paulo",
+  linkedin_url: "https://www.linkedin.com/in/danilocecci",
+  rocketseat_profile: "https://app.rocketseat.com.br/me/danilocecci"
+}
