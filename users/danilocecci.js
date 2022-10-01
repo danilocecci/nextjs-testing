6e86474e2e86197aab7003ada4d5b18d16eb3d56
Valdemir Filho <filhux@gmail.com>
2022-10-01 06:42:25 -0300

    Del incomplete info Danillo.js

--- users/danilocecci.js
+++
@@ -1,9 +0,0 @@
-export const data = {
-  name: "Danilo Cecci",
-  email: "danilocecci@gmail.com",
-  github: "https://github.com/danilocecci",
-  city: "Andradina",
-  state: "São Paulo",
-  linkedin_url: "https://www.linkedin.com/in/danilocecci",
-  rocketseat_profile: "https://app.rocketseat.com.br/me/danilocecci"
-}
